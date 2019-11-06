6e6c383dc8853843375d56209ec3aadead52ed24
Divino Silva <fernandinho_07@hotmail.com>
2019-11-06 08:48:14 -0200

    Add oauth linkedin

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
-
+LINKEDIN_ID = Rails.application.config_for(:linkedin)['app_id']
+LINKEDIN_SECRET = Rails.application.config_for(:linkedin)['app_secret']
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  require 'omniauth-linkedin'
+  config.omniauth :linkedin, LINKEDIN_ID, LINKEDIN_SECRET
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :products
   resources :ratings
   get '/auth/:provider/callback', to: 'oauth#callback', as: 'oauth_callback'
